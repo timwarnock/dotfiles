fddb08fe32ab54a9b3fd6a2c08d93d69f8edfa61
Tim Warnock <tim@timwarnock.com>
2019-11-12 18:50:25 -0500

    gather command, default to "gather left"

--- bin/gather.sh
+++ bin/gather.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 ##
 ## get a list of all windows (not the Desktop itself)
 WIN_IDS=$(wmctrl -l | grep -v 'Desktop$' | awk '{ print $1 }')
 
 ##
-## move each window
+## move each window LEFT 0,0
 for winid in $WIN_IDS; do
   wmctrl -ir $winid -e 0,0,0,-1,-1
 done
