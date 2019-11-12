08304a16fefce246c513e756384a264adc417ad9
Tim Warnock <tim@timwarnock.com>
2019-11-12 18:38:06 -0500

    gather_windows.sh

---
+++ bin/gather_windows.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+##
+## get a list of all windows (not the Desktop itself)
+WIN_IDS=$(wmctrl -l | grep -v 'Desktop$' | awk '{ print $1 }')
+
+##
+## move each window
+for winid in $WIN_IDS; do
+  wmctrl -ir $winid -e 0,0,0,-1,-1
+done
+
