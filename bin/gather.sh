a17e74e7921964f4189e6ca79c7b55fa3c8d707c
Tim Warnock <tim@timwarnock.com>
2019-11-12 21:55:50 -0500

    gather LEFT | RIGHT | CENTER

--- bin/gather.sh
+++ bin/gather.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
 ##
-## get a list of all windows (not the Desktop itself)
-WIN_IDS=$(wmctrl -l | grep -v 'Desktop$' | awk '{ print $1 }')
+## LEFT | RIGHT | CENTER | SET | RESET
+BASEDIR=$(dirname "$0")
+LAST_LAST=$BASEDIR/.gather.last
+
+CMD="SET"
+case "${1^^}" in
+    "RIGHT" | "R") CMD="RIGHT" ;;
+    "CENTER" | "C") CMD="CENTER" ;;
+    "LEFT" | "L") CMD="LEFT" ;;
+    "RESET") echo "reset is not implemented yet";
+        CMD="LEFT" ;;
+    *) echo "set is not implemented yet";
+        CMD="LEFT" ;;
+esac
+
 
 ##
-## move each window LEFT 0,0
-for winid in $WIN_IDS; do
-  wmctrl -ir $winid -e 0,0,0,-1,-1
+## get full desktop width (across all displays)
+WIDTH=$(wmctrl -d | awk '{ print $4 }' | awk -Fx '{ print $1 }')
+
+##
+## move all windows LEFT | RIGHT | CENTER
+WIN_POS=$(wmctrl -lG | grep -v 'Desktop$' | awk '{ print($1, $3, $4, $5, $6) }')
+echo "$WIN_POS" | while read -r WIN_ID WIN_X WIN_Y WIN_W WIN_H; do
+  COORDS="0,0"
+  if [ "$CMD" = "RIGHT" ]; then
+    let X_COORD=$(( $WIDTH - $WIN_W ))
+    COORDS="$X_COORD,0"
+  elif [ "$CMD" = "CENTER" ]; then
+    let CENTER_X=$(( $WIDTH / 2 ))
+    let W_HALF=$(( $WIN_W / 2 ))
+    let X_COORD=$(( $CENTER_X - $W_HALF ))
+    COORDS="$X_COORD,0"
+  fi
+  wmctrl -ir $WIN_ID -e 0,$COORDS,-1,-1
 done
 
